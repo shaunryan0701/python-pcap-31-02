82651739940333d9906ed717a0de1846b2c18443
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-03-06 11:30:22 +0000

    Added inheritance.py

--- inheritance.py
+++ inheritance.py
@@ -1,11 +1,16 @@
 class Vehicle:
-    pass
+    def __init__(self, speed):
+        self.speed = speed
 
 class LandVehicle(Vehicle):
-    pass
+    def __init__(self, speed, wheeel_count):
+        Vehicle.__init__(self, speed)
+        self.wheel_count = wheeel_count
 
 class Car(LandVehicle):
     pass
 
 
+my_car = Car(50, 4)
 print(issubclass(Car, LandVehicle))
+print(my_car.__dict__)
