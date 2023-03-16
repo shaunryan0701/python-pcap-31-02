93e2047ada77b7bcd9da655d925c0c86da5a4bd8
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-03-16 10:43:03 +0000

    Added some methods to file_read

--- file_read.py
+++ file_read.py
@@ -16,3 +16,13 @@ try:
 except Exception as e:
     print('An Error has occurred', e)
 
+try:
+    stream = open('animals.txt')
+    character = stream.read(1)
+    while character != '':
+      print(character, end='-')
+      character = stream.read(1)
+    
+    stream.close()
+except Exception as e:
+    print('An Error has occurred', e)
