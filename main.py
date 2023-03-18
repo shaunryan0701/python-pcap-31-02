40e95a2f22a92a48004ba4222adbaa33a87576c1
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-03-18 20:54:11 +0000

    added modules

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-import strings_utils
+# import strings_utils
 
-quotes = ['Being happy never goes out of style.',
-'Life is either a great adventure or nothing.',
-'All you need in this life is ignorance and confidence; then success is sure.',
-'All your life, you will be faced with a choice. You can choose love or hate... I choose love.',
-'The time is always right to do what is right.']
+# quotes = ['Being happy never goes out of style.',
+# 'Life is either a great adventure or nothing.',
+# 'All you need in this life is ignorance and confidence; then success is sure.',
+# 'All your life, you will be faced with a choice. You can choose love or hate... I choose love.',
+# 'The time is always right to do what is right.']
 
-print(strings_utils.halve_strings(quotes))
+# print(strings_utils.halve_strings(quotes))
+
+import sys
+
+sys.path.insert(0, './modules')
+import module
+
+for p in sys.path:
+    print(p)
+
+zeroes = [0 for i in range(5)]
+ones = [1 for i in range(5)]
+
+print(module.prodl(ones))
+print(module.suml(zeroes))
