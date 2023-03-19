f566490abaa8db77e57f275212041c287f8d1c6f
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-03-19 18:47:48 +0000

    amendment

--- strings.py
+++ strings.py
@@ -3,10 +3,10 @@ sample_story = '''Once I'm awaken, I'll sacrifice your soul to the ruler of dark
 def get_logest_word(story):
     words = story.replace('.', '').replace(',', '').split()
     print(words)
-    max_length_word = ''
+    max_len_word = ''
     for word in words:
-        if len(word) > len(max_length_word):
-            max_length_word = word
-    return max_length_word       
+        if len(word) > len(max_len_word):
+            max_len_word = word
+    return max_len_word       
 
 print(get_logest_word(sample_story))
