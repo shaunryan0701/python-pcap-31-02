91e1b04e113f0cafeb036fed9b3e366ecbb54685
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-03-26 16:13:47 +0100

    Added generator.py

--- generator.py
+++ generator.py
@@ -10,3 +10,17 @@ for v in powers_of_2(16):
 
 t = [x for x in powers_of_2(20)]
 print(t)
+
+
+def fibonacci(n):
+    p = pp = 1
+    for i in range(n):
+        if i in [0, 1]:
+            yield 1
+        else:
+            n = p + pp
+            pp, p = p, n
+            yield n
+
+fibs = list(fibonacci(10))
+print(fibs)
