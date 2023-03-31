f925aab46c012302e94f3ee6bee0d600f647e11a
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-04-01 00:58:29 +0100

    Updated file_read.py

--- file_read.py
+++ file_read.py
@@ -38,4 +38,38 @@ try:
 except Exception as e:
     print('An Error has occurred', e)
 
-    
+
+# read()
+from os import strerror
+
+try:
+    cnt = 0
+    s = open('text.txt', "rt")
+    content = s.read()
+    for ch in content:
+        print(ch, end='')
+        cnt += 1
+    s.close()
+    print("\n\nCharacters in file:", cnt)
+except IOError as e:
+    print("I/O error occurred: ", strerr(e.errno))
+
+# readline()
+from os import strerror
+
+try:
+    ccnt = lcnt = 0
+    s = open('text.txt', 'rt')
+    line = s.readline()
+    while line != '':
+        lcnt += 1
+        for ch in line:
+            print(ch, end='')
+            ccnt += 1
+        line = s.readline()
+    s.close()
+    print("\n\nCharacters in file:", ccnt)
+    print("Lines in file:     ", lcnt)
+except IOError as e:
+    print("I/O error occurred:", strerror(e.errno))
+
