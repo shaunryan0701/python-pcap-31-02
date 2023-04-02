fe3a3eca970855f883476ef4a7b490383db6fbeb
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-04-02 10:35:30 +0100

    Updated file_read.py

--- file_read.py
+++ file_read.py
@@ -107,3 +107,15 @@ try:
 	print("Lines in file:     ", lcnt)
 except IOError as e:
 	print("I/O error occurred: ", strerror(e.errno))
+
+
+# writing to files 
+try:
+	fo = open('newtext.txt', 'wt') # A new file (newtext.txt) is created.
+	for i in range(10):
+		s = "line #" + str(i+1) + "\n"
+		for ch in s:
+			fo.write(ch)
+	fo.close()
+except IOError as e:
+	print("I/O error occurred: ", strerror(e.errno))
