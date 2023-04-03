3bc9309dc30a861cd75760cd5bf6374bd7d7d5cf
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-04-03 09:41:52 +0100

    Updated file_write.py

--- file_write.py
+++ file_write.py
@@ -5,3 +5,14 @@ try:
     stream.close()
 except Exception as e:
     print('An error has occurred:', e)
+
+# stderror
+from os import strerror
+
+try:
+    fo = open('newtext.txt', 'wt')
+    for i in range(10):
+        fo.write("line #" + str(i+1) + "\n")
+    fo.close()
+except IOError as e:
+    print("I/O error occurred: ", strerror(e.errno))
