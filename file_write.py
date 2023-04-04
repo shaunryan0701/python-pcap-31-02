4725ebdec17826367d3e6f607538af747a676b0c
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-04-04 23:16:53 +0100

    Updated file_write.py

--- file_write.py
+++ file_write.py
@@ -25,3 +25,39 @@ for i in range(len(data)):
 
 for b in data:
     print(hex(b))
+
+
+data = bytearray(10)
+
+for i in range(len(data)):
+    data[i] = 10 + i
+
+try:
+    bf = open('file.bin', 'wb')
+    bf.write(data)
+    bf.close()
+except IOError as e:
+    print("I/O error occurred:", strerror(e.errno))
+
+
+try:
+    bf = open('file.bin', 'rb')
+    bf.readinto(data)
+    bf.close()
+
+    for b in data:
+        print(hex(b), end=' ')
+except IOError as e:
+    print("I/O error occurred:", strerror(e.errno))
+
+
+try:
+    bf = open('file.bin', 'rb')
+    data = bytearray(bf.read())
+    bf.close()
+
+    for b in data:
+        print(hex(b), end=' ')
+
+except IOError as e:
+    print("I/O error occurred:", strerror(e.errno))
