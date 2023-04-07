4f0cda8e3719ea343a08731f1ea004ebcd421624
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-04-07 14:52:54 +0100

    Updated file_write.py

--- file_write.py
+++ file_write.py
@@ -61,3 +61,10 @@ try:
 
 except IOError as e:
     print("I/O error occurred:", strerror(e.errno))
+
+
+import os
+
+os.makedirs("my_first_directory/my_second_directory")
+os.chdir("my_first_directory")
+print(os.listdir())
