a5297238d46ccc7b5bb0e2cdb7a516ea3ca1c217
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-04-08 22:07:45 +0100

    Updated file_write.py

--- file_write.py
+++ file_write.py
@@ -68,3 +68,9 @@ import os
 os.makedirs("my_first_directory/my_second_directory")
 os.chdir("my_first_directory")
 print(os.listdir())
+
+os.makedirs("my_first_directory/my_second_directory")
+os.chdir("my_first_directory")
+print(os.getcwd())
+os.chdir("my_second_directory")
+print(os.getcwd())
