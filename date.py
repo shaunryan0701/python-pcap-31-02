ce0e732653173cb84055a770284b6cbaa0e5d9d2
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-04-10 13:59:57 +0100

    Updated date.py

--- date.py
+++ date.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime
 
 today = date.today()
 
@@ -6,3 +6,14 @@ print("Today:", today)
 print("Year:", today.year)
 print("Month:", today.month)
 print("Day:", today.day)
+
+
+dt = datetime(2019, 11, 4, 14, 53)
+
+print("Datetime:", dt)
+print("Date:", dt.date())
+print("Time:", dt.time())
+
+print("today:", datetime.today())
+print("now:", datetime.now())
+print("utcnow:", datetime.utcnow())
