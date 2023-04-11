31e46981ed20b3b1883caedda45d4e8f1d8460cb
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-04-11 08:48:09 +0100

    Updated date.py

--- date.py
+++ date.py
@@ -17,3 +17,19 @@ print("Time:", dt.time())
 print("today:", datetime.today())
 print("now:", datetime.now())
 print("utcnow:", datetime.utcnow())
+
+from datetime import date
+from datetime import datetime
+
+d1 = date(2020, 11, 4)
+d2 = date(2019, 11, 4)
+
+print(d1 - d2)
+
+dt1 = datetime(2020, 11, 4, 0, 0, 0)
+dt2 = datetime(2019, 11, 4, 14, 53, 0)
+
+print(dt1 - dt2)
+
+delta = timedelta(weeks=2, days=2, hours=3)
+print(delta)
