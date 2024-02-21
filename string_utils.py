f7256067c9eaf60690c25e90cf8cc7260a8a3071
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2024-02-21 19:55:51 +0000

    Updated  string_utils example

--- string_utils.py
+++
@@ -1,6 +0,0 @@
-import math
-
-def halve_string(input_string):
-    first_half = input_string[: math.ceil(len(input_string)/2)]
-    second_half = input_string[math.ceil(len(input_string)/2) :]
-    return (first_half, second_half)
