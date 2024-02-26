a8b0c4e2546e98d60bc0c3937568950a5171b465
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2024-02-26 09:13:39 +0000

    Updated strings_utils example

--- strings_utils.py
+++
@@ -1,8 +0,0 @@
-import string_utils
-
-def halve_strings(string_list):
-    halved_strings = []
-    for string_element in string_list:
-        halved_strings.append(string_utils.halve_string(string_element))
-
-    return halved_strings
